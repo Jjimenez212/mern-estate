c916f131a7e8837a2f02d772ab7c7db6ea9c2a64
Juan Miguel Jimenez <bozz212@gmail.com>
2024-06-16 14:08:43 -0700

    Backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from './pages/About';
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
+
+
 function App() {
   return (
-    <BrowserRouter className="text-3xl font-bold underline">
+    <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home  />} />
         <Route path="/sign-in" element={<SignIn  />} />
